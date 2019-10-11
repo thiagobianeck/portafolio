aad6dc3fd53530d135fcd2c9a76cb7482f37b653
Thiago Bianeck <thiagobianeck@gmail.com>
2019-10-11 14:57:28 -0300

    "Template pronto com rotas - Fim da seção 3"

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: PortafolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'item', component: ItemComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes, { useHash: true })
+  ],
+  exports: [
+    RouterModule,
+  ]
+})
+export class AppRoutingModule {}
